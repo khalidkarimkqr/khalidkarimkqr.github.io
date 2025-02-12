c0317e3ab202f236de2b90e3de5ac96d33a03405
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-12 06:45:41 -0800

    added navigation links in the centre and styled them with hover effects

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -4,6 +4,9 @@ const NavbarLogo = () => {
   return (
     <div>
       <h1 className="text-white  text-2xl sm:hidden md:block">Khalid Karim</h1>
+      <h1 className="text-white font-special font-extrabold text-4xl sm:block md:hidden ">
+        KK
+      </h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "350px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     colors: {
       brown: "#53423e",
       lightBrown: "#645550",
