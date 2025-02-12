d23ce0f7341f89115c85ff2a5e6de0b510079e96
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-12 04:50:08 -0800

    Added basic navbar structure with three divisions: logo, links and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavbarMain from "./components/navbar/NavbarMain";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <NavbarMain />
+    </main>
+  );
 }
 
 export default App;
