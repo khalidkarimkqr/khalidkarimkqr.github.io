930c4d970851fdb10f989c8a7d34476f914d14e5
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-13 03:18:50 -0800

    used the fadeIn function in bio section and my skills section accurately

--- src/components/bio/BioPic.jsx
+++ src/components/bio/BioPic.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 import { PiHexagonThin } from "react-icons/pi";
 
 const BioPic = () => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <motion.div
+      variants={fadeIn("left", 0.2)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0 }}
+      className="h-full flex items-center justify-center"
+    >
       <img
         src="../../images/HexaPic.png"
         alt="Khalid karim"
@@ -13,7 +21,7 @@ const BioPic = () => {
       {/* <div className="absolute -z-10 flex justify-center items-center animate-pulse">
         <PiHexagonThin className=" md:h-[100%] sm:h-[100%] min-h-[600px] w-auto text-orange opacity-70  animate-[spin_20s_linear_infinite] " />
       </div> */}
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/bio/BioText.jsx
+++ src/components/bio/BioText.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
+
 const BioText = () => {
   return (
     <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center">
-      <h2 className="lg:text-2xl sm:text-xl  uppercase text-lightGrey ">
+      <motion.h2
+        variants={fadeIn("down", 0.2)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="lg:text-2xl sm:text-xl  uppercase text-lightGrey "
+      >
         Software Developer
-      </h2>
-      <h1 className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold font-special uppercase">
+      </motion.h2>
+      <motion.h1
+        variants={fadeIn("right", 0.4)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold font-special uppercase"
+      >
         Khalid Karim
-      </h1>
-      <p className="text-lg mt-4 text-white">
+      </motion.h1>
+      <motion.p
+        variants={fadeIn("up", 0.6)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="text-lg mt-4 text-white"
+      >
         Hi, I’m Khalid Karim, an aspiring software developer with a passion
         <br /> for building innovative solutions through technology. I have
         hands-on <br /> experience in web development and machine learning
@@ -21,7 +42,7 @@ const BioText = () => {
         developers, <br /> contribute to meaningful projects, and learn from the
         vibrant tech <br />
         community.
-      </p>
+      </motion.p>
     </div>
   );
 };
