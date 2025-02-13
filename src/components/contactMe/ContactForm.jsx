204282e48a96a2d2800eb3caae5a4b59dc0720fe
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-13 00:17:52 -0800

    tested the form using emailjs and worked smoothly

--- src/components/contactMe/ContactForm.jsx
+++ src/components/contactMe/ContactForm.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_j7q34vb", "template_j54i5d8", form.current, {
+        publicKey: "-zCSVnEXaVXohTDc3",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+  return (
+    <div>
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="from_name"
+          placeholder="Your Name"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+        />
+        <input
+          type="email"
+          name="from_email"
+          placeholder="Your Email"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+        />
+        <textarea
+          type="text"
+          name="message"
+          rows="9"
+          cols="50"
+          placeholder="Message"
+          required
+          className=" rounded-lg bg-lightBrown p-2"
+        />
+        <button
+          type="submit"
+          className="w-full rounded-lg border border-cyan text-white h-12 font-bold text-xl hover:bg-darkCyan bg-cyan transition-all duration-500"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
--- src/components/contactMe/ContactInfo.jsx
+++ src/components/contactMe/ContactInfo.jsx
@@ -6,7 +6,7 @@ import SingleInfo from "./SingleInfo";
 
 const ContactInfo = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 text-white">
       <SingleInfo text="khalid13878@hotmail.com" Image={HiOutlineMail} />
       <SingleInfo text="+12363398382" Image={FiPhone} />
       <SingleInfo text="Burnaby, BC" Image={IoLocationOutline} />
