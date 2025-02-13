4e45cecacb8cc8c436aab506fdd3c86e05831387
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-12 18:27:04 -0800

    completed the top section with image handling and formatting

--- src/components/experience/ExperienceTopMiddle.jsx
+++ src/components/experience/ExperienceTopMiddle.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ExperienceTopMiddle = () => {
-  return <div>ExperienceTopMiddle</div>;
+  return (
+    <div className="lg:w-[40%] md:w-[50%] sm:w-[80%]">
+      <img src="../../images/experience-image.png" alt="my experience" />
+    </div>
+  );
 };
 
 export default ExperienceTopMiddle;
