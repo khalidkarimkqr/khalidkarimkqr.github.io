bbd696095f5310b06161b2b6f088366d2527cc56
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-13 00:33:30 -0800

    Linked my socials to link tags

--- src/components/contactMe/ContactMeLeft.jsx
+++ src/components/contactMe/ContactMeLeft.jsx
@@ -4,7 +4,7 @@ import ContactForm from "./ContactForm";
 
 const ContactMeLeft = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-8 w-full">
       <div>
         <h2 className="text-orange text-3xl mb-4">Get In Touch</h2>
         <p className="text-white">
--- src/components/contactMe/ContactSocial.jsx
+++ src/components/contactMe/ContactSocial.jsx
@@ -7,9 +7,18 @@ import { FaInstagram } from "react-icons/fa";
 const ContactSocial = () => {
   return (
     <div className="flex gap-4">
-      <SingleContactSocial link="#" Icon={FaLinkedinIn} />
-      <SingleContactSocial link="#" Icon={FiGithub} />
-      <SingleContactSocial link="#" Icon={FaInstagram} />
+      <SingleContactSocial
+        link="https://www.linkedin.com/in/khalidkarim24/"
+        Icon={FaLinkedinIn}
+      />
+      <SingleContactSocial
+        link="https://github.com/khalidkarimkqr"
+        Icon={FiGithub}
+      />
+      <SingleContactSocial
+        link="https://www.instagram.com/khalid_kqr/"
+        Icon={FaInstagram}
+      />
     </div>
   );
 };
