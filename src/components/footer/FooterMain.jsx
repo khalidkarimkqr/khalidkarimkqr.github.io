a3db2ba7425517b562255f3efc0425e57c997919
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-13 02:06:13 -0800

    Added the navigation linksto eery section with hover effects and styled it

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
+const links = [
+  { link: "About Me", section: "about" },
+  { link: "Skills", section: "skills" },
+  { link: "Experience", section: "experience" },
+  { link: "Projects", section: "projects" },
+  { link: "Contact", section: "contact" },
+];
+
 const FooterMain = () => {
   return (
-    <div>
-      <div></div>
-      <div>
-        <p>Khalid Karim</p>
-        <ul></ul>
+    <div className="px-4">
+      <div className="w-full h-[1px] bg-lightGrey mt-24"></div>
+      <div className="md:flex justify-between mt-4 max-w-[1200px] mx-auto sm:hidden">
+        <p className="text-3xl text-lightGrey ">Khalid Karim</p>
+        <ul className="flex gap-4 text-lightGrey text-xl">
+          {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <a
+                  href="#"
+                  className="hover:text-white transition-all duration-500 cursor-pointer"
+                >
+                  {item.link}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
       <p></p>
     </div>
