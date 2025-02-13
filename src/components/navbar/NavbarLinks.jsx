5da35c6a83fef92afefcba10501c44b09d1ef7fd
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-13 02:32:54 -0800

    Added the smooth scroll functionality on the navigation links

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const links = [
   { link: "About Me", section: "about" },
@@ -14,12 +15,16 @@ const NavbarLinks = () => {
       {links.map((link, index) => {
         return (
           <li key={index} className="group">
-            <a
-              href="#"
+            <Link
+              to={link.section}
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-130}
               className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
             >
               {link.link}
-            </a>
+            </Link>
             <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
           </li>
         );
