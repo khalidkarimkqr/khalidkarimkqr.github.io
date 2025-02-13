7d9bb9e33ec68d1b3ad1cf34c732695e12e23573
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-12 18:42:57 -0800

    completed the singleExperience cards with formatting and styling.

--- src/components/experience/AllExperience.jsx
+++ src/components/experience/AllExperience.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import SingleExperience from "./SingleExperience";
+import { FaArrowRightLong } from "react-icons/fa6";
+// import { motion } from "framer-motion";
+// import { fadeIn } from "../../framerMotion/variants";
+
+const experiences = [
+  {
+    job: "Back End Developer",
+    company: "Horizon Expeditions",
+    date: "Oct 2024 - Dec 2024",
+    responsibilities: [
+      "Implementing reusable components.",
+      "Participating in large scale application.",
+      "Working on the performance of web applications.",
+      "Generating new ideas for better user experience.",
+    ],
+  },
+  {
+    job: "Full Stack Developer",
+    company: "CJSF 90.1 Radio",
+    date: "Nov 2024 - Present",
+    responsibilities: [
+      "Explaining and facilitating web development concepts.",
+      "Help students with their assignments and grade them weekly.",
+      "Provide support for students through their learning journey.",
+    ],
+  },
+  {
+    job: "Math Instructor",
+    company: "Mathnasium",
+    date: "Nov 2024 - Present",
+    responsibilities: [
+      "Teaching JavaScript, React and TailwindCSS.",
+      "Participating in preparing course materials.",
+      "Helping students through their way in learning web development technologies.",
+    ],
+  },
+];
 
 const AllExperience = () => {
-  return <div>AllExperience</div>;
+  return (
+    <div className="flex md:flex-row sm:flex-col items-center justify-between">
+      {experiences.map((experience, index) => {
+        return (
+          <>
+            <SingleExperience key={index} experience={experience} />
+            {index < 2 ? (
+              <FaArrowRightLong className="text-6xl text-orange lg:block sm:hidden" />
+            ) : (
+              ""
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AllExperience;
--- src/components/experience/ExperienceTopMiddle.jsx
+++ src/components/experience/ExperienceTopMiddle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ExperienceTopMiddle = () => {
   return (
-    <div className="lg:w-[40%] md:w-[50%] sm:w-[80%]">
+    <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
       <img src="../../images/experience-image.png" alt="my experience" />
     </div>
   );
