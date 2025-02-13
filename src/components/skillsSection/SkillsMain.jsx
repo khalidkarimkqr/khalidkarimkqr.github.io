65f3dce8d2b9cbb4670589494dcfc27b291f3faf
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-12 16:57:25 -0800

    added hover effects on each skills prop to make it look nice

--- src/components/skillsSection/SkillsMain.jsx
+++ src/components/skillsSection/SkillsMain.jsx
@@ -4,9 +4,13 @@ import AllSkills from "./AllSkills";
 
 const SkillsMain = () => {
   return (
-    <div>
-      <SkillsText />
-      <AllSkills />
+    <div id="skills">
+      <div className="max-w-[1200px] px-4 mx-auto min-h-[600px] relative overflow-hidden">
+        <SkillsText />
+        <div className="bottom-[50px] absolute left-[50%] -translate-x-[50%] sm:hidden lg:block">
+          <AllSkills />
+        </div>
+      </div>
     </div>
   );
 };
