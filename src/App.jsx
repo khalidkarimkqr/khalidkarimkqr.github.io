60a3b66720eae13890bce2f9707d1fec7cf7dbf2
khalidkarimkqr <khalid13878@hotmail.com>
2025-02-13 01:59:10 -0800

    Implemented the structure for the footer and added my name on the left side

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SubSkills from "./components/skillsSection/SubSkills";
 import ExperienceMain from "./components/experience/ExperienceMain";
 import ProjectsMain from "./components/projects/ProjectsMain";
 import ContactMeMain from "./components/contactMe/ContactMeMain";
+import FooterMain from "./components/footer/FooterMain";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <ExperienceMain />
       <ProjectsMain />
       <ContactMeMain />
+      <FooterMain />
       <HelperSection />
     </main>
   );
---
+++ src/components/footer/FooterMain.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FooterMain = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <p>Khalid Karim</p>
+        <ul></ul>
+      </div>
+      <p></p>
+    </div>
+  );
+};
+
+export default FooterMain;
